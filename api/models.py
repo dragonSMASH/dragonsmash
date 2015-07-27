db43cbdbbac682d4c3dd4b8006e3aed7bd705125
Shreyas Chand <shreyas.chand@gmail.com>
2015-07-26 22:18:26 -0700

    Use django auth model. Small model fixups.

--- api/models.py
+++ api/models.py
@@ -1,16 +1,15 @@
-from django.contrib.gis.db import models # inherits from django.db.models
+from django.contrib.gis.db import models  # inherits from django.db.models
+from django.contrib.auth.models import User as BaseUser
 from django.core.validators import RegexValidator
 
 PhoneRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-        message="Phone number must be entered in the format: '+999999999'. \
-                Up to 15 digits allowed.")
+                            message="Phone number must be entered in the format: '+999999999'. \
+                                     Up to 15 digits allowed.")
 
 
 class User(models.Model):
-    username = models.CharField(max_length=25, unique=True)
-    email = models.EmailField()
+    user = models.OneToOneField(BaseUser)
     phone = models.CharField(max_length=15, validators=[PhoneRegex])
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class UserData(models.Model):
@@ -18,20 +17,21 @@ class UserData(models.Model):
     location = models.PointField(geography=True)
     time = models.DateTimeField()
     money = models.IntegerField()
+
     objects = models.GeoManager()
 
 
 class Dragon(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    # various base stats
+    # TODO: implement various base stats
 
 
 class UserDragon(models.Model):
     user = models.ForeignKey(User)
     dragon = models.ForeignKey(Dragon)
     experience = models.IntegerField()
-    # various stats
     last_laid = models.DateTimeField()
+    # TODO: implement various stats
 
 
 class Effect(models.Model):
@@ -49,5 +49,7 @@ class Egg(models.Model):
     user = models.ForeignKey(User)
     dragon = models.ForeignKey(UserDragon)
     location = models.PointField(geography=True)
-    # various stats
     lays_dragon_type = models.ForeignKey(Dragon)
+    # TODO: implement various stats
+
+    objects = models.GeoManager()

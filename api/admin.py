22ceef8438bc5610bafa451d02577a576916e5e0
Shreyas Chand <shreyas.chand@gmail.com>
2015-07-29 22:35:43 -0700

    Model fixups

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,7 @@ from .models import *
 admin.site.register(Player)
 admin.site.register(PlayerData)
 admin.site.register(Dragon)
-admin.site.register(UserDragon)
+admin.site.register(PlayerDragon)
 admin.site.register(Effect)
-admin.site.register(DragonEffect)
 admin.site.register(Egg)
 
--- api/models.py
+++ api/models.py
@@ -1,38 +1,63 @@
 from django.contrib.gis.db import models  # inherits from django.db.models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from django.db import transaction
 
 PhoneRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                             message="Phone number must be entered in the format: '+999999999'. \
                                      Up to 15 digits allowed.")
 
 
-class Player(models.Model):
+class BaseModel(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(BaseModel, self).save(*args, **kwargs)
+
+
+class Player(BaseModel):
     user = models.OneToOneField(User, related_name="player")
     phone = models.CharField(max_length=15, validators=[PhoneRegex], unique=True)
 
+    class PlayerManager(models.Manager):
+        @transaction.atomic()
+        def create_player(self, username, password, phone, email=None):
+            user = User(username=username)
+            if email:
+                user.email = email
+            user.set_password(password)
+            user.full_clean()
+            user.save()
 
-class PlayerData(models.Model):
-    player = models.OneToOneField(Player)
+            player = self.create(user=user, phone=phone)
+            return player
+
+    objects = PlayerManager()
+
+
+class PlayerData(BaseModel):
+    player = models.OneToOneField(Player, related_name="data")
     location = models.PointField(geography=True)
-    time = models.DateTimeField()
     money = models.IntegerField()
 
     objects = models.GeoManager()
 
 
-class Dragon(models.Model):
+class Dragon(BaseModel):
     name = models.CharField(max_length=50, unique=True)
     # TODO: implement various base stats
 
 
-class Effect(models.Model):
+class Effect(BaseModel):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
 
 
-class UserDragon(models.Model):
-    player = models.ForeignKey(Player)
+class PlayerDragon(BaseModel):
+    player = models.ForeignKey(Player, related_name='player')
     dragon = models.ForeignKey(Dragon)
     experience = models.IntegerField()
     last_laid = models.DateTimeField()
@@ -40,9 +65,9 @@ class UserDragon(models.Model):
     # TODO: implement various stats
 
 
-class Egg(models.Model):
+class Egg(BaseModel):
     player = models.ForeignKey(Player)
-    dragon = models.ForeignKey(UserDragon)
+    dragon = models.ForeignKey(PlayerDragon)
     location = models.PointField(geography=True)
     lays_dragon_type = models.ForeignKey(Dragon)
     # TODO: add time

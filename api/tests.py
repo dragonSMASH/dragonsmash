7b3ceb30ca55da1b0028f2b41ab5ef5dd3c6f85f
Shreyas Chand <shreyas.chand@gmail.com>
2015-07-29 22:48:27 -0700

    Add tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from api.models import *
 
-# Create your tests here.
+
+class PlayerModelTests(TestCase):
+
+    def test_simple_player_creation(self):
+        Player.objects.create_player("shreyas", "password", "5105555555")
+
+    def test_player_duplicate_username_not_allowed(self):
+        Player.objects.create_player("shreyas", "passwprd", "5105555555")
+        with self.assertRaises(ValidationError):
+            Player.objects.create_player("shreyas", "passwprd", "5105555554")
+
+    def test_player_duplicate_phone_not_allowed(self):
+        Player.objects.create_player("shreyas", "passwprd", "5105555555")
+        with self.assertRaises(ValidationError):
+            Player.objects.create_player("marvin", "passwprd", "5105555555")

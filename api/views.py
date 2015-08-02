7cc6c66e0ac79aeed5283a78229e6d6a04172d3d
Shreyas Chand <shreyas.chand@gmail.com>
2015-08-01 22:06:45 -0700

    Add convenience decorators for api get/post only views. reorganize views

--- api/views.py
+++ api/views.py
@@ -6,28 +6,16 @@ from rest_framework.views import exception_handler
 from .serializers import RegisterPlayerResource
 
 
-@api_view(['GET'])
-@permission_classes((AllowAny,))
-def status(request):
-    response_data = {'message': 'howdy, partner!'}
-    return Response(response_data)
+# Convenience decorators for get-only and post-only view
+def api_get_view(func):
+    return api_view()(func)
 
 
-@api_view(['POST'])
-@permission_classes((AllowAny,))
-def register(request):
-    register_info = RegisterPlayerResource(data=request.data)
-    if register_info.is_valid(raise_exception=True):
-        player = register_info.save()
-        return Response({"player_id": player.id, "auth_token": player.user.auth_token.key})
+def api_post_view(func):
+    return api_view(['POST'])(func)
 
 
-@api_view(['POST'])
-def logout(request):
-    request.user.auth_token.delete()
-    return Response({"message": "logout successful"})
-
-
+# API-wide exception handler (Set in config.settings.base)
 def handle_exception(exception, context):
     """ Return well-formed and consistent response in case any exceptions occur """
     response = exception_handler(exception, context)
@@ -45,3 +33,26 @@ def handle_exception(exception, context):
 
     response.data = data
     return response
+
+
+# Main API Views
+@api_get_view
+@permission_classes((AllowAny,))
+def status(request):
+    response_data = {'message': 'howdy, partner!'}
+    return Response(response_data)
+
+
+@api_post_view
+@permission_classes((AllowAny,))
+def register(request):
+    register_info = RegisterPlayerResource(data=request.data)
+    if register_info.is_valid(raise_exception=True):
+        player = register_info.save()
+        return Response({"player_id": player.id, "auth_token": player.user.auth_token.key})
+
+
+@api_post_view
+def logout(request):
+    request.user.auth_token.delete()
+    return Response({"message": "logout successful"})

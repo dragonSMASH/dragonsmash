2b4b8ac26f00b2a260ccbc142d6e239b66973e52
Shreyas Chand <shreyas.chand@gmail.com>
2015-08-02 01:20:17 -0700

    Add lat/long to register endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from .models import Player
@@ -9,12 +10,18 @@ class RegisterPlayerResource(serializers.Serializer):
     password = serializers.CharField()
     email = serializers.EmailField(required=False)
     phone = serializers.CharField()
+    latitude = serializers.FloatField()
+    longitude = serializers.FloatField()
 
     def create(self, validated_data):
         player = Player.objects.create_player(validated_data.pop('username'),
                                               validated_data.pop('password'),
                                               validated_data.pop('phone'),
                                               validated_data.pop('email', None))
+        PlayerData.objects.create(player=player,
+                                  location=Point(validated_data.pop('longitude'),
+                                                 validated_data.pop('latitude'),
+                                                 srid=4326),
+                                  money=0)
         Token.objects.create(user=player.user)
-
         return player

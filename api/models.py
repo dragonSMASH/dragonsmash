3a4f8ae3d04ba4a0f2d7572ff733388cf6f3c7a7
zhangmarvin <zhangmarvin95@gmail.com>
2015-08-02 01:56:42 -0700

    add newline for style purposes

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import transaction
 
+
 PhoneRegex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                             message="Phone number must be entered in the format: '+999999999'. \
                                      Up to 15 digits allowed.")

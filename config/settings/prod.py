fcd24a8d46c8bfc49002cc9ff55faae0e61fed12
Shreyas Chand <shreyas.chand@gmail.com>
2015-08-02 01:49:48 -0700

    Add basic prod settings

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,1 +1,10 @@
 from .base import *
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
